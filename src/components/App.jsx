19ad1bdc182e9ff49fd202b00a712cc9828f93d0
VirLg <19virlg@gmail.com>
2023-07-13 17:13:15 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Form from './Form/Form';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  handleChange = data => {
+    this.setState({
+      name: data,
+    });
+  };
+
+  addContact = () => {
+    const { contacts, name } = this.state;
+    const check = contacts.filter(el => el.name === name);
+    if (check === [])
+      this.state(prev => {
+        console.log(name);
+        return {
+          contacts: [...prev, ...name],
+        };
+      });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Form handleChange={this.handleChange} addContact={this.addContact} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+class Form extends Component {
+  handleChange = data => {
+    this.props.handleChange({
+      name: data.target.value,
+    });
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.addContact(100);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {};
+
+export default Form;
